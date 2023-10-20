dc15fa7b5ff406afeacafa8f676c1686e0f0eea6
asdwerte1 <samjmarkham@outlook.com>
2023-10-20 20:29:00 +0100

    updated values to a dict insteadof list, created list from user input with each digit separated\n TODO NOW - use list and dictionary to print out the LED display of each number

--- LEDDisplay.py
+++ LEDDisplay.py
@@ -7,22 +7,24 @@ def led_display():
         led_display()
 
     #create a list of the values to print
-    values = [
-        "  #\n" * 5,
-        "###\n  #\n###\n#  \n###",
-        "###\n  #\n###\n  #\n###",
-        "# #\n# #\n###\n  #\n  #",
-        "###\n#  \n###\n  #\n###",
-        "###\n#  \n###\n# #\n###",
-        "###\n  #\n  #\n  #\n  #",
-        "###\n# #\n###\n# #\n###",
-        "###\n# #\n###\n  #\n###",
-        "###\n# #\n# #\n# #\n###"
-    ]
+    values = {
+        "1": "  #\n" * 5,
+        "2": "###\n  #\n###\n#  \n###",
+        "3": "###\n  #\n###\n  #\n###",
+        "4": "# #\n# #\n###\n  #\n  #",
+        "5": "###\n#  \n###\n  #\n###",
+        "6": "###\n#  \n###\n# #\n###",
+        "7": "###\n  #\n  #\n  #\n  #",
+        "8": "###\n# #\n###\n# #\n###",
+        "9": "###\n# #\n###\n  #\n###",
+        "0": "###\n# #\n# #\n# #\n###"
+    }
 
-    for i in values:
-        print(i)
-        print("")
+    #Take user input and cast it to a list
+    stringInput = str(num)
+    numList = []
+    for i in stringInput:
+        numList.append(i)
 
 
 led_display()
