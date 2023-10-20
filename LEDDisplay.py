e4fe7089c7c2f18ddafe83c4537cb787eebffb8c
asdwerte1 <samjmarkham@outlook.com>
2023-10-20 20:52:11 +0100

    Final working product - use row-by-row approach to print numbers into single line

--- LEDDisplay.py
+++ LEDDisplay.py
@@ -1,34 +1,35 @@
 def led_display():
     try:
-        #User input number
+        # User input number
         num = int(input("Please enter a number: "))
     except ValueError:
         print("Value entered is not a number")
         led_display()
 
-    #create a list of the values to print
+    # Create a list of the values to print
     values = {
-        "1": "  #\n  #\n  #\n  #\n  #",
-        "2": "###\n  #\n###\n#  \n###",
-        "3": "###\n  #\n###\n  #\n###",
-        "4": "# #\n# #\n###\n  #\n  #",
-        "5": "###\n#  \n###\n  #\n###",
-        "6": "###\n#  \n###\n# #\n###",
-        "7": "###\n  #\n  #\n  #\n  #",
-        "8": "###\n# #\n###\n# #\n###",
-        "9": "###\n# #\n###\n  #\n###",
-        "0": "###\n# #\n# #\n# #\n###"
+        "1": "  #  \n  #  \n  #  \n  #  \n  #  ",
+        "2": "###  \n  #  \n###  \n#    \n###  ",
+        "3": "###  \n  #  \n###  \n  #  \n###  ",
+        "4": "# #  \n# #  \n###  \n  #  \n  #  ",
+        "5": "###  \n#    \n###  \n  #  \n###  ",
+        "6": "###  \n#    \n###  \n# #  \n###  ",
+        "7": "###  \n  #  \n  #  \n  #  \n  #  ",
+        "8": "###  \n# #  \n###  \n# #  \n###  ",
+        "9": "###  \n# #  \n###  \n  #  \n###  ",
+        "0": "###  \n# #  \n# #  \n# #  \n###  "
     }
 
-    #Take user input and cast it to a list
+    # Take user input and cast it to a list
     stringInput = str(num)
     numList = []
     for i in stringInput:
         numList.append(values[i])
 
-    #Output the values
-    for i in numList:
-        print(i, end=" ")
-
+    # Output the values on one line
+    for row in range(5):
+        for i in numList:
+            print(i.split('\n')[row], end="  ")
+        print()
 
 led_display()
